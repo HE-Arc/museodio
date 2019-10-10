8944235a496738a4a99ece1cf6761e888d6f4d05
Luca Verardo <luca.verardo@pindex.ch>
2019-10-10 16:19:35 +0200

    Added : - Navbar - Register button - Login button

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -11,21 +11,41 @@
   <link rel="stylesheet" href="{{asset('vendor\leaflet\leaflet\leaflet.css')}}">
   <link rel="stylesheet" href="{{asset('css/museodio.css')}}">
   <link rel="stylesheet" href="{{asset('css/materialize-css/materialize.css')}}">
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
   <script src="{{asset('js/materialize-css/materialize.js')}}" charset="utf-8"></script>
 
 </head>
 <body>
 
-  <div class="header">
-    {{-- TODO : Transform this into a section --}}
-    <h1>Museodio</h1>
-    <a class="waves-effect waves-light btn">button</a>
+  <div class="header-container">
+    <div class="header">
+      {{-- TODO : Transform this into a section --}}
+      <nav class="main-nav-bar">
+        <div class="nav-wrapper">
+          <a href="#!" class="brand-logo mus-title">Museodio</a>
+          <ul class="right hide-on-med-and-down">
+            <li><a class="waves-effect waves-light btn modal-trigger" href="#modal1">Modal</a></li>
+            <li><a class="waves-effect waves-light btn">Register <i class="material-icons right">person_add</i></a></li>
+            <li><a class="waves-effect waves-light btn">Sign in <i class="material-icons right">input</i></a></li>
+          </ul>
+        </div>
+      </nav>
+    </div>
   </div>
 
-  <div id="map">
 
+  <div id="map"></div>
+
+
+  <div id="loginModal" class="modal">
+    <div class="modal-content">
+      <h4>Modal Header</h4>
+      <p>A bunch of text</p>
+    </div>
+    <div class="modal-footer">
+      <a href="#!" class="modal-close waves-effect waves-green btn-flat">Agree</a>
+    </div>
   </div>
-
 
   <script type="text/javascript">
 
@@ -42,7 +62,7 @@
       maxZoom: 20
     }).addTo(mainMap);
 
-    mainMap.zoomControl.setPosition('topright');
+    mainMap.zoomControl.setPosition('bottomleft');
 
     displayCurrentUserPosition();
   }
@@ -61,9 +81,10 @@
 
   }
 
-window.onload = function(){
-  initMap();
-};
+  window.onload = function(){
+    initMap();
+    M.AutoInit();
+  };
 </script>
 <script src="{{asset('vendor\leaflet\leaflet\leaflet.js')}}" charset="utf-8"></script>
 </body>
