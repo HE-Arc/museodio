44414df7543edbd951932b40fc03c06329b717dc
Zegorax <luca@verardo.ch>
2019-10-28 15:17:57 +0100

    🔧🐛 Fixed composer update in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-#composer update
+composer update
 php artisan vendor:publish --tag=public --force
-php artisan migrate:fresh
+php artisan migrate
 php artisan db:seed
 php artisan passport:install
-
