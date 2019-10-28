4b9f2f410af0a815066b4af0ed7e532ed0519c8e
Zegorax <luca@verardo.ch>
2019-10-28 16:14:48 +0100

    💄Removed automatic marker opening

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -25,6 +25,12 @@
   @include('modals.register')
   @include('modals.signin')
 
+  <div class="fixed-action-btn">
+  <a class="btn-floating btn-large action-buttons-color">
+    <i class="large material-icons">add</i>
+  </a>
+</div>
+
   <script type="text/javascript">
   var mainMap = null;
 
@@ -82,7 +88,6 @@
       L.marker([audioNote.latitude, audioNote.longitude], {icon: playIcon})
         .addTo(mainMap)
         .bindPopup(customPopup)
-        .openPopup();
     }
   }
 
