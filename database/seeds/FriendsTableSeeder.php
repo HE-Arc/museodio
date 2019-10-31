0298568d7832faca3adadd22b282f44d33cd3eac
roxane.carraux <roxane.carraux@he-arc.ch>
2019-10-31 14:20:29 +0100

    fix seeding friends

---
+++ database/seeds/FriendsTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Friends;
+
+class FriendsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $data =
+        [
+            [
+                'user_id_1'=>1,
+                'user_id_2'=>2
+            ],
+            [
+                'user_id_1'=>1,
+                'user_id_2'=>3
+            ]
+        ];
+
+        foreach ($data as $key => $value) {
+           Friends::create($value);
+       }
+    }
+}
