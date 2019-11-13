cb8b2b5d916f86e737cff20f8445546cae0863cb
roxane.carraux <roxane.carraux@he-arc.ch>
2019-11-13 17:04:48 +0100

    clean friends page

--- resources/views/friends.blade.php
+++ resources/views/friends.blade.php
@@ -2,24 +2,6 @@
 <div>Friends</div>
 <br>
 <div>
-    @foreach ($users as $user)
-        <div>
-            {{$user->id}} - {{$user->firstname}} {{$user->lastname}} ------- <button>Demander</button> <button>Accepter</button> <button>Refuser</button> <button>Supprimer</button>
-        </div>
-    @endforeach
-</div>
-<br>
-<div>
-    @foreach ($friends as $friend)
-        <div>
-            {{App\Friends::getUserById($friend->user_id_1)->firstname}} {{App\Friends::getUserById($friend->user_id_1)->lastname}} -
-            {{App\Friends::getUserById($friend->user_id_2)->firstname}} {{App\Friends::getUserById($friend->user_id_2)->lastname}}
-            ------- <button>Demander</button> <button>Accepter</button> <button>Refuser</button> <button>Supprimer</button>
-        </div>
-    @endforeach
-</div>
-<br>
-<div>
     @foreach ($fr as $f)
         @if($f->isAccepted)
             <div>
