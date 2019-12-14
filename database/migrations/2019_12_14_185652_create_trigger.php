fad89f1b0c8957ce104679739e1c5a63b059de6c
JonasFreibur <jonas@TIC17-FREIBURGJ.intra.eiaj.ch>
2019-12-14 21:08:17 +0100

    fixed two way friend request migration

--- database/migrations/2019_12_14_185652_create_trigger.php
+++ database/migrations/2019_12_14_185652_create_trigger.php
@@ -14,12 +14,12 @@ class CreateTrigger extends Migration
     public function up()
     {
         DB::unprepared('
-        CREATE TRIGGER tr_friends_double_accepted AFTER INSERT ON `friends`
+        CREATE TRIGGER tr_friends_double_accepted AFTER UPDATE ON `friends`
             FOR EACH ROW BEGIN
-                IF ROW.user_id_1 = NEW.user_id_1 AND ROW.user_id_2 = NEW.user_id_2 THEN
+                IF OLD.user_id_1 = NEW.user_id_1 AND OLD.user_id_2 = NEW.user_id_2 THEN
                     UPDATE friends SET isAccepted = `1` WHERE user_id_1 = NEW.user_id_1 AND user_id_2 = NEW.user_id_2;
                 END IF;
-                IF ROW.user_id_2 = NEW.user_id_1 AND ROW.user_id_1 = NEW.user_id_2 THEN
+                IF OLD.user_id_2 = NEW.user_id_1 AND OLD.user_id_1 = NEW.user_id_2 THEN
                     UPDATE friends SET isAccepted = `1` WHERE user_id_1 = NEW.user_id_2 AND user_id_2 = NEW.user_id_1;
                 END IF;
             END;
