1e303e8547c92f91686e037014b0efd5508c8f33
Zegorax <luca@verardo.ch>
2019-12-16 20:24:59 +0100

    Added sandwich menu for mobile-only

---
+++ resources/views/sidenavLoginRegister.blade.php
@@ -0,0 +1,5 @@
+<ul id="sideNavLoginRegister" class="sidenav main-sidenav">
+  <li><h2 style="margin-left: 10px;">Museodio</h2></li>
+  <li><a class="waves-effect waves-light modal-trigger" onclick="closeAllModals(); setFocus('reg_firstName');" href="#registerModal">Register <i class="material-icons right icon-white">person_add</i></a></li>
+  <li><a class="waves-effect waves-light modal-trigger" onclick="closeAllModals(); setFocus('log_email');" href="#loginModal">Sign in <i class="material-icons right icon-white">input</i></a></li>
+</ul>
