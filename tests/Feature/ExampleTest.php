ded3c989f3cbad3fdf51d2171a4b694769e9fbaa
Zegorax <luca@verardo.ch>
2019-12-16 20:30:16 +0100

    Fixing failing test

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -15,7 +15,7 @@ class ExampleTest extends TestCase
     public function testBasicTest()
     {
         $response = $this->get('/');
-
+        echo $response->content();
         $response->assertStatus(200);
     }
 }
