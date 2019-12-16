00c1ac41fef22193b15c4303d59851fff8b343c2
Zegorax <luca@verardo.ch>
2019-12-16 19:06:58 +0100

    Added mobile fixes

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -2,8 +2,8 @@
   <div class="header">
     <nav class="main-nav-bar">
       <div class="nav-wrapper">
-        <a href="#!" class="brand-logo mus-title">Museodio</a>
-        <ul class="right hide-on-med-and-down">
+        <a href="#!" class="brand-logo mus-title left">Museodio</a>
+        <ul class="right ">
           @unless (Auth::check())
           <li><a class="waves-effect waves-light btn modal-trigger action-buttons-color" onclick="closeAllModals(); setFocus('reg_firstName');" href="#registerModal">Register <i class="material-icons right">person_add</i></a></li>
           <li><a class="waves-effect waves-light btn modal-trigger action-buttons-color" onclick="closeAllModals(); setFocus('log_email');" href="#loginModal">Sign in <i class="material-icons right">input</i></a></li>
